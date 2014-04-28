1ce7e0b87210bdcf5c49408e7ca107dc8c391994
MrLarssonJr <jesper.ekvik@telia.com>
2014-04-28 23:00:30 +0200

    Kickstarted to get familliar with the engine.

--- FlagHolder/src/insertName/flagHolder/FlagHolder.java
+++ FlagHolder/src/insertName/flagHolder/FlagHolder.java
@@ -1,8 +1,18 @@
 package insertName.flagHolder;
 
+import java.awt.*;
+import java.util.*;
+
+import Engine.*;
+
 public class FlagHolder {
 	public static void main(String[] args) {
-		System.out.println("hej");
+		ArrayList<Scene> scenes = new ArrayList<Scene>();
+		scenes.add(new Scene(Color.BLACK, new Dimension(400, 400)));
+		
+		Engine.getInstance(scenes);
+		
+		Engine.startGame();
 	}
 	
 }
